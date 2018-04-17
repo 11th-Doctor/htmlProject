500296a4e4a9fc5215cae73533fd5282d12ffe63
Daryl Zhang <august9382@gmail.com>
2018-04-17 16:55:50 +0000

    Added main js file and buttons of the table

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    function Book() {
+        this.ISBN;
+        this.bookName;
+        this.author;
+        this.publicationDate
+        this.publisher;
+    }
+    
+    
+});
