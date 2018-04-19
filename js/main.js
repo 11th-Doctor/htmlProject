154572fc49962196f827af4f21958f5ba08df2bc
Daryl Zhang <august9382@gmail.com>
2018-04-19 13:25:50 +0000

    Adding records funtion

--- js/main.js
+++ js/main.js
@@ -1,11 +1,22 @@
+
 $(function () {
-    function Book() {
-        this.ISBN;
-        this.bookName;
-        this.author;
-        this.publicationDate
-        this.publisher;
-    }
     
+    $("#formOfAdding").submit(function() {
+        
+        if (typeof(Storage) !== "undefined") {
+            var rows = {
+                isbn:$("#isbn").val(),
+                bookName:$("#bookName").val(),
+                author:$("#author").val(),
+                publicationDate:$("#publicationDate").val(),
+                price:$("#price").val()
+            }
+            
+            localStorage.setItem("isbn",JSON.stringify(rows));
     
+        } else {
+            document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
+        }
+    });
+      
 });
