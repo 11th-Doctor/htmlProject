243ee7e9f326a54809dfb99007285a289588edc3
Daryl Zhang <august9382@gmail.com>
2018-04-19 14:21:30 +0000

    Function of showRecord

--- js/main.js
+++ js/main.js
@@ -12,11 +12,34 @@ $(function () {
                 price:$("#price").val()
             }
             
-            localStorage.setItem("isbn",JSON.stringify(rows));
-    
+            localStorage.setItem(rows.isbn,JSON.stringify(rows));
+            
         } else {
             document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
         }
     });
       
+     
+    function showRecord(isbn) {
+        
+        // Generating td
+        var bookList = JSON.parse(localStorage.getItem(isbn));
+        var isbn = "<td>" + bookList.isbn + "</td>";
+        var bookName = "<td>" + bookList.bookName + "</td>";
+        var author = "<td>" + bookList.author + "</td>";
+        var publicationDate = "<td>" + bookList.publicationDate + "</td>";
+        var price = "<td>" + bookList.price + "</td>";
+        var buttons = "<td>" + '<button class="btn btn-success">修改</button>' 
+            + ' <button class="btn btn-danger">刪除</button>';
+        
+        // Generating tr
+        var tTr = $("<tr></tr>").append(isbn).append(bookName).append(author)
+        .append(publicationDate).append(price).append(buttons);
+        
+        // Appending tr to tbody
+        $("#mainTable tbody").append(tTr);
+        console.log(tTr);
+    }  
+    
+    //showRecord("1111");
 });
